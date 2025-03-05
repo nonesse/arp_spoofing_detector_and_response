a0daf7301aeae602b3b8fb2da9acd5816716e4d0
nonesse <>
2025-03-05 17:15:27 +0100

    Enhance functionality
    New Features:
    
       - Mitigate the effects of ARP spoofing on any machine within your local network (Windows or Linux).
       - Increase reliability by responding only with trusted MAC addresses, thereby avoiding amplification of spoofing attacks.
       - Added packet count argument (-p) to customize the response power by choosing the number of packets sent during each detection.

--- README.md
+++ README.md
@@ -1,54 +1,90 @@
 <div align="center">
 
-    <img width="250px" src="logo.png"/>
+    <img width="275px" src="logo.png"/>
 
 </div>
 
 # ARP Spoofing Detector and Response
 
-This Python script is designed to detect and respond to ARP spoofing attacks in Linux systems within a local network. It utilizes the Scapy library to sniff ARP packets and compare the MAC addresses to identify potential spoofing attempts.
+This Python script is designed to detect and respond to ARP spoofing attacks within a local network. It utilizes the Scapy library to sniff ARP packets and compare the MAC addresses to identify potential spoofing attempts.
 
 ## Features
 
 - Detects ARP spoofing attempts by monitoring ARP packets.
 - Logs detailed information about detected spoofing attempts.
-- Restores local ARP cache entries to mitigate spoofing effects.
+- Restores local ARP cache entries to revert spoofing effects.
+- Mitigates the effects of ARP spoofing by sending packets to any machine under attack within your local network (Windows or Linux).
+- Increases reliability by responding only with trusted MAC addresses, thereby avoiding amplification of spoofing attacks.
 - Color-coded logging for better visibility in the console.
-- Save logs information in a .log file
+- Save logs information in a .log file.
 
 ## Requirements
 
 - Python 3.x
-- Scapy library
-- Access to run commands with `sudo` to sniff packets and modify local ARP cache
+- Scapy library.
+- Execute the script on a Linux machine.
+- Run the script with elevated privileges using `sudo` to sniff and send packets and modify local ARP cache.
 
 You can install the required library using pip:
 
 ```bash
 pip install scapy
 ```
+
+## Args
+
+The following arguments can be used when running the script:
+
+- `-p`, `--packets`:
+  - **Type**: `int`
+  - **Description**: Number of ARP packets to send to mitigate the attack during each detection.
+  - **Default value**: `10`
+  - **Minimum value**: `1`
+  - **Maximum value**: `1000`
+  - **Usage**: Specify the number of packets to send. For example, `-p 20` will send 20 ARP packets.
+
 ## Usage
 
-1. **Prepare Known Devices**: Create a known_devices.json file in the same directory as the script. This file can either be empty or not exist at all, but for optimal performance, it is recommended to include a list of known devices with their IP and MAC addresses. Having this information allows the script to more accurately detect ARP spoofing attempts:
+1. **Prepare Known Devices**: Create a `known_devices.json` file in the same directory as the script. This file can be:
 
- ```json
-[
-    {
-        "ip_address": "192.168.55.1",
-        "mac_address": "11:22:33:44:55:66"
-    },
-    {
-        "ip_address":"192.168.55.2",
-        "mac_address":"77:88:99:AA:BB:CC"
-    }
-]
-```
+   - **Empty or Nonexistent**: Use this option if you want to run the script only as a detector.
+   - **Filled**: Include a list of known devices within your local network with their IP and MAC addresses. This allows the script to function as both a detector and a response tool to mitigate ARP spoofing attempts.
 
-2. **Run the Script**: Execute the script with Python. You will need to run it with elevated privileges to allow it to sniff packets and modify local ARP cache.
+   Here is an example of how the `known_devices.json` file should look:
 
-	`sudo python arp_spoofing_detector_and_response.py`
+    ```json
+    [
+        {
+            "ip_address": "192.168.55.1",
+            "mac_address": "11:22:33:44:55:66"
+        },
+        {
+            "ip_address":"192.168.55.2",
+            "mac_address":"77:88:99:AA:BB:CC"
+        }
+    ]
+    ```
+
+2. **Run the Script**: Execute the script with Python. You will need to run it with elevated privileges to allow it to sniff and send packets and modify local ARP cache.
+
+    ```bash    
+    sudo python arp_spoofing_detector_and_response.py
+    ```
+
+    Or with `-p` option:
+
+    ```bash    
+    sudo python arp_spoofing_detector_and_response.py -p 20
+    ```
+    In this example, the script will send 20 ARP packets to mitigate the attack during each detection. If the `-p` option is not specified, the default value of 10 packets will be used.
 
 3. **Monitor Output**: The script will start sniffing ARP packets and log any detected spoofing attempts. The logs will be saved in a file named with the current timestamp.
+
+## Output example:
+<div align="center">
+    <img  src="output_example.png"/>
+</div>
+
 ## Logging
 
 The script uses a custom logging configuration that outputs messages to both the console and a log file. The log messages are color-coded based on their severity level, but the script only displays at two levels, `WARNING` and `INFO`:
--- arp_spoofing_detector_and_response.py
+++ arp_spoofing_detector_and_response.py
@@ -1,8 +1,9 @@
 from scapy.all import *
+from datetime import datetime
 import logging
 import os
-from datetime import datetime
 import uuid
+import argparse
 
 class ColoredFormatter(logging.Formatter):
     RESET = "\033[0m"
@@ -37,15 +38,16 @@ class ARPSpoofingDetectorAndResponse:
     POSSIBLE_ARP_SPOOFING_MSG = "Posible ARP Spoofing detected." + DETAILS_MSG
     ARP_SPOOFING_DETECTION_MSG = "ARP Spoofing detected." + DETAILS_MSG 
 
-    def __init__(self):
+    def __init__(self, packet_count):
         self.logging_configuration()
         self.known_devices_file_path = "known_devices.json"
         self.known_devices = self.load_known_devices(self.known_devices_file_path)
         self.local_ip = self.get_local_ip()
         self.local_mac = self.get_local_mac()
+        self.packet_count = self.get_packets_count(packet_count)
 
     def logging_configuration(self):
-        """Initialize the logging tool
+        """Initializes the logging tool
         """
         current_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
         log_file_name = f"{current_time}_arp_spoofing_dr_log.log"
@@ -61,14 +63,14 @@ class ARPSpoofingDetectorAndResponse:
             ])
 
     def load_known_devices(self, file_path):
-        """Load the IP and MAC of known devices from a json file
+        """Loads the IP and MAC of known devices from a json file
 
         Args:
             file_path (str): path of the json file to be load
 
         Returns:
-            known_devices {(str):(str)}: dictionary with key "ip_address" and value "mac_address"
-                """
+            {(str):(str)}: dictionary with key "ip_address" and value "mac_address"
+        """
         known_devices = {}
         try:
             with open(file_path, 'r') as f:
@@ -78,21 +80,38 @@ class ARPSpoofingDetectorAndResponse:
                 for device in data:
                     ip = device['ip_address'].strip()
                     mac = device['mac_address'].strip()
-                    known_devices[ip] = mac
+                    known_devices[ip] = self.convert_mac_into_valid_format(mac)
                     
         except FileNotFoundError:
-            print(f"File {self.known_devices_file_path} doesn't exists")
-            print("No known devices have been recognized")
+            self.log_info(f"File {self.known_devices_file_path} doesn't exists")
+            self.log_info("No known devices have been recognized")
         except Exception:
-            print("No known devices have been recognized")
+            self.log_info("No known devices have been recognized")
         
         return known_devices
 
+    def convert_mac_into_valid_format(self, mac_address):
+        """Converts a MAC address into a valid format (e.g., XX:XX:XX:XX:XX:XX)
+
+        Args:
+            mac_address (str): The MAC address to convert
+
+        Returns:
+            (str): The MAC address in a valid format
+        """
+        invalid_mac_separator = '-'
+        valid_mac_separator = ':'
+
+        if invalid_mac_separator in mac_address:
+            mac_address = mac_address.replace(invalid_mac_separator, valid_mac_separator)
+        
+        return mac_address
+    
     def get_local_ip(self):
         """Obtains the local IP address
 
         Returns:
-            local_ip (str): Local IP address
+            (str): Local IP address
         """
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -103,27 +122,57 @@ class ARPSpoofingDetectorAndResponse:
         return local_ip
 
     def get_local_mac(self):
-        """Obtains the local MAC address reliably
+        """Reliably retrieves the local MAC address
         
         Returns
             (str): Local MAC address
         """
         return ':'.join(("%012x" % uuid.getnode())[i:i+2] for i in range(0,12,2))
 
+    def get_packets_count(self, packet_count):
+        """Validates and adjust the number of packets specified by the user.
+
+        This method checks the provided packet_count against predefined limits.
+        If packet_count is None or below the minimum allowed, it sets it to a default value.
+        If packet_count exceeds the maximum allowed, it logs a message and adjusts it to the maximum.
+
+        Args:
+            packets_count (int or None): The desired number of packets to send.
+
+        Returns:
+            (int): The validated number of packets, adjusted if necessary.
+        """
+        if packet_count is None or packet_count < MIN_PACKETS_COUNT:
+            packet_count = DEFAULT_PACKETS_COUNT
+    
+        elif packet_count > MAX_PACKETS_COUNT:
+            self.log_info(f"The maximum number of packets allowed is {MAX_PACKETS_COUNT}. Adjusting packet count to {MAX_PACKETS_COUNT}.")
+            packet_count = MAX_PACKETS_COUNT
+        
+        return packet_count
+       
+    def log_info(self, message):
+        """Displays a info log message
+
+        Args:
+            (str): Message to be displayed
+        """
+        logging.info(message)  
+
     def start_detection(self):
-        """Capture ARP packets for process them for ARP Spoofing
+        """Captures ARP packets for process them for ARP Spoofing
         """
         try:
-            print("Searching for ARP Spoofing...")
+            self.log_info("Starting detection. Searching for ARP Spoofing...")
             sniff(prn=self.process_packet, filter="arp", store=0)
         except KeyboardInterrupt:
-            print("Stopping ARP Spoofing detection...")
+            self.log_info("Stopping ARP Spoofing detection...")
 
     def process_packet(self, packet):
-        """Process ARP packets for ARP Spoofing detection
+        """Processes ARP packets for ARP spoofing detection
 
-        Compare the actual MAC address of an IP with the MAC address from the ARP packet for that IP.
-        Display alerts through logging
+        Compares the actual MAC address of an IP with the MAC address from the ARP packet for that IP
+        Displays alerts through logging
 
         Args:
             packet (scapy.packet): Packet to be processed
@@ -131,113 +180,114 @@ class ARPSpoofingDetectorAndResponse:
         is_arp_reply_packet =  packet.haslayer(ARP) and packet[ARP].op == 2
         if is_arp_reply_packet:
 
-            source_ip = packet[ARP].psrc
             destination_ip = packet[ARP].pdst
+            source_ip = packet[ARP].psrc
+            destination_mac = packet[ARP].hwdst
             source_mac = packet[ARP].hwsrc
-            actual_mac = self.get_mac(source_ip)
+            actual_mac, is_reliable_mac = self.get_mac(source_ip)
 
             if (actual_mac is not None) and (actual_mac.lower() != source_mac.lower()):
+                if is_reliable_mac:
+                    self.log_warning(self.ARP_SPOOFING_DETECTION_MSG.format(destination_ip, source_ip, source_mac, actual_mac))
 
-                if self.local_ip == destination_ip:
-                    self.i_am_the_target(impersonated_ip=source_ip, false_mac=source_mac, actual_mac=actual_mac)
+                    if self.local_ip == destination_ip:
+                        self.restore_local_arp_cache(host_ip=source_ip, host_mac=actual_mac)
 
-                elif self.local_ip == source_ip:
-                    self.someone_is_trying_to_impersonate_me(destination_ip=destination_ip, destination_mac=packet[ARP].hwdst, false_mac=source_mac)
-
+                    else:    
+                        self.send_arp_packets(destination_ip=destination_ip, destination_mac=destination_mac, host_ip=source_ip, host_mac=actual_mac) 
                 else:
                     self.log_warning(self.POSSIBLE_ARP_SPOOFING_MSG.format(destination_ip, source_ip, source_mac, actual_mac))
-     
+        
     def get_mac(self, ip):
-        """Get the MAC address for a given IP address
+        """Retrieves the MAC address for a given IP address and determines its reliability
         
         Args:
             ip (str): IP address for which the MAC address is to be determined
 
         Returns:
-            (str) MAC address of the provided IP
+            tuple:
+                - (str) MAC address of the provided IP
+                - (bool) Flag to indicate the reliability of the MAC
         """
         if ip == self.local_ip:
-            return self.local_mac
+            return self.local_mac, True
         
         elif ip in self.known_devices:
-            return self.known_devices[ip]
+            return self.known_devices[ip], True
         
         else: 
-            answer, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout = 3, verbose = 0)
-            if answer:
-                return answer[0][1].src
-        
-    def i_am_the_target(self, impersonated_ip, false_mac, actual_mac):
-        """Restore local ARP cache for counter attack ARP Spoofing attack
+            return self.get_mac_from_arp_response(ip), False
+    
+    def get_mac_from_arp_response(self, ip):
+        """Retrieves the MAC address from an ARP response for a given IP address.
 
         Args:
-            impersonated_ip (str): IP address that the attacker is trying to impersonate
-            false_mac (str): MAC address that the attacker is trying to bind to the impersonated_ip
-            actual_mac (str): Actual MAC address of the impersonated_ip
-        """
+            ip (str): The IP address to query.
 
-        self.log_warning(self.ARP_SPOOFING_DETECTION_MSG.format(self.local_ip, impersonated_ip, false_mac, actual_mac))
-        self.restore_local_arp_cache(host_ip=impersonated_ip, host_mac=actual_mac)
+        Returns:
+            (str): MAC address in system format.
+        """    
+        answer, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout = 3, verbose = 0)
+        if answer:
+            return answer[0][1].src
+        
+        return None
 
     def log_warning(self, message):
-        """Display a warning log message
+        """Displays a warning log message
 
         Args:
-            message (str): Message to be displayed
+            (str): Message to be displayed
         """
         logging.warning(message)
 
     def restore_local_arp_cache(self, host_ip, host_mac):
-        """Restore local ARP cache deleting the host_ip entry and writting it again
+        """Restores local ARP cache deleting the host_ip entry and writting it again
 
         Args:
-            host_ip (str): IP address for the host to be written to the local cache.
-            host_mac (str): Actual MAC address of host_ip.
+            host_ip (str): IP address for the host to be written to the local cache
+            host_mac (str): Actual MAC address of the device with the host_ip
         """
         os.system(f"sudo arp -d {host_ip}")
         os.system(f"sudo arp -s {host_ip} {host_mac} temp")
-        self.log_info(f"Restored local ARP cache: {host_ip} is at {host_mac}")
-        
-    def log_info(self, message):
-        """Display a info log message
-
-        Args:
-            message (str): Message to be displayed
-        """
-        logging.info(message)
-
-    def someone_is_trying_to_impersonate_me(self, destination_ip, destination_mac, false_mac):
-        """Send ARP packets to the destination_ip trying to restore our MAC reference in destination machine
-
-        Args:
-            destination_ip (str): IP address for the device where the local MAC address reference will be restored.
-            destination_mac (str): MAC address of destination_ip device.
-            false_mac (str): MAC address that the attacker is trying to bind to our local machine's IP
-        """
-        self.log_warning(self.ARP_SPOOFING_DETECTION_MSG.format(destination_ip, self.local_ip, false_mac,self.local_mac))
-        self.send_arp_packets(destination_ip=destination_ip, destination_mac=destination_mac)
+        self.log_info(f"Restored local ARP cache: {host_ip} is at {host_mac}")  
     
-    
-    def send_arp_packets(self, destination_ip, destination_mac):
-        """Send ARP packets for restore local MAC address reference in destination machine
+    def send_arp_packets(self, destination_ip, destination_mac, host_ip, host_mac):
+        """Sends ARP packets to destination_ip
 
         Args:
-            destination_ip (str): IP address for the device where the local MAC address reference will be restored.
-            destination_mac (str): MAC address of destination_ip device.
+            destination_ip (str): IP address of the device where the host_mac address reference will be restored
+            destination_mac (str): MAC address of the device with the destination_ip
+            host_ip (str): IP address of the device being impersonated
+            host_mac (str): Actual MAC address of the host device
         """
-        local_ip = self.local_ip
-        local_mac = self.local_mac
-
         arp_response = ARP(
                 pdst=destination_ip,
                 hwdst=destination_mac,
-                psrc=local_ip,
-                hwsrc=local_mac,
+                psrc=host_ip,
+                hwsrc=host_mac,
                 op='is-at'
             )
-        sendp(Ether(dst=destination_mac)/arp_response, verbose=0, count=50)
-        self.log_info(f"ARP packets sent to {destination_ip}: {local_ip} is at {local_mac}")
+        sendp(Ether(dst=destination_mac)/arp_response, verbose=0, count=self.packet_count)
+        self.log_info(f"{self.packet_count} ARP packets sent to {destination_ip}: {host_ip} is at {host_mac}")
+
+DEFAULT_PACKETS_COUNT = 10
+MIN_PACKETS_COUNT = 1
+MAX_PACKETS_COUNT = 1000
 
 if __name__ == "__main__":
-    arp_spoofing_detector = ARPSpoofingDetectorAndResponse()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-p', '--packets',
+        type=int, 
+        help=
+            "Number of ARP packets to send to mitigate the attack during each detection. "
+            f"Default value: {DEFAULT_PACKETS_COUNT}, "
+            f"Min. value: {MIN_PACKETS_COUNT}, "
+            f"Max. value: {MAX_PACKETS_COUNT}")
+    
+    args = parser.parse_args()
+    packets_count = args.packets
+
+    arp_spoofing_detector = ARPSpoofingDetectorAndResponse(packets_count)
     arp_spoofing_detector.start_detection()
